ab3edb16e258a34d9fa37259062da19289c7c14d
Andrew Chan-Quach <110324991+achanquach@users.noreply.github.com>
2024-02-19 18:42:29 +1100

    Added an isOdd function
    A standard modulo checker returning a boolean given parsed an integer

--- Main.java
+++ Main.java
@@ -2,4 +2,11 @@ public class Main {
     public static void main(String[] args){
         System.out.println("Hello World");
     }
+
+    public boolean isOdd(int x){
+        if(x % 2 == 0){
+            return false;
+        } 
+        return true;
+    }
 }
